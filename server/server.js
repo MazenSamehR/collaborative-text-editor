13b1b6a1e8b0e23057f2956fc4ddc6c5d9919d1d
MazenSamehR <103901861+MazenSamehR@users.noreply.github.com>
2025-10-25 19:25:44 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const Document = require("./Documnet");
 const express = require("express");
 
 mongoose.connect(
-  "mongodb+srv://pesfifa200020:IlA7qdORhg5AED5x@cluster0.fi3wq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  "YOUR-CONNECTION-STRING"
 );
 
 const io = require("socket.io")(3001, {
